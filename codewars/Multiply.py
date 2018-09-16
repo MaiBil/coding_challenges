484115252fac988d8b597d2c9d75c973b923be24
MBilsky <mai.bilsky@gmail.com>
2018-09-16 18:28:47 -0300

    Add Multiply.py

---
+++ codewars/Multiply.py
@@ -0,0 +1,22 @@
+#!/user/bin/env python
+
+# author: Mailen Bilsky
+# date: 11/09/2018
+
+# title: Multiply
+# url: https://www.codewars.com/kata/50654ddff44f800200000004
+# difficulty: 8 kyu
+# time complexity: O(n), with n being the amount of numbers in the input string
+
+def multiply(a, b):
+    """
+    Multiply two numbers. Return the result.
+
+    :param nmultiply: Two ints
+    :type multiply: int, int
+    :return: Return the result of multiplying a * b
+    :rtype: int
+    """
+
+  c = a * b
+  return c
