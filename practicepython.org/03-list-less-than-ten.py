ea1b894b303566333302fb65bc9ecd36f9c10ac5
MBilsky <mai.bilsky@gmail.com>
2018-10-03 01:28:20 -0300

    Change so that it prints a list with the result

--- practicepython.org/03-list-less-than-ten.py
+++ practicepython.org/03-list-less-than-ten.py
@@ -13,11 +13,18 @@ def less_than_5(lst):
 
 	  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 	and write a program that prints out all the elements of the list that are less than 5.
+
+	Extras:
+
+	Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
 	"""
 
+  ls = []
   for i in lst:
     if i < 5:
-      print(i)
-
+      ls.append(i)
+  print(ls)
+    
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 less_than_5(a)
+
