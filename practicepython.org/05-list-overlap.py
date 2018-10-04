96f4f70290b8f3ef7a2a72ea0157e287b30af7b0
MBilsky <mai.bilsky@gmail.com>
2018-10-04 00:33:12 -0300

    Add 05-list-overlap.py

---
+++ practicepython.org/05-list-overlap.py
@@ -0,0 +1,20 @@
+"""
+Take two lists, say for example these two:
+
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+"""
+
+
+def common_list(list1, list2):
+    if len(list1) > len(list2):
+        ls = list1
+    else:
+        ls = list2
+    return list(sorted(set([x for x in ls if x in list1 and x in list2])))
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print(common_list(a, b))
