e956bcdaad13adbaf13b1432ff6726f21c4c9661
MBilsky <mai.bilsky@gmail.com>
2018-10-06 20:35:38 -0300

    Add changes requested in PR

--- practicepython.org/03-list-less-than-ten.py
+++ practicepython.org/03-list-less-than-ten.py
@@ -7,24 +7,35 @@
 # url: https://www.practicepython.org/exercise/2014/02/15/03-list-less-than-ten.html
 # time complexity: O(n), with n being the amount of numbers in the input string
 
+"""
+Take a list, say for example this one:
+
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+and write a program that prints out all the elements of the list that are
+less than 5.
+
+Extras:
+
+1. Instead of printing the elements one by one, make a new list that has all
+the elements less than 5 from this list in it and print out this new list.
+2. Write this in one line of Python.
+3. Ask the user for a number and return a list that contains only elements
+from the original list a that are smaller than that number given by the
+user.
+"""
+
+
 def less_than_num(lst):
-	"""
-	Take a list, say for example this one:
+    """
+    :param nmultiply: A list
+    :type multiply: list
+    :return: Returns a list with all the elements from the input list that
+    are smaller than the number given by the user
+    :rtype: string
+    """
+    return [x for x in lst if x < int(num)]
 
-	  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-	and write a program that prints out all the elements of the list that are less than 5.
-
-	Extras:
-
-	Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
-	Write this in one line of Python.
-	Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
-	"""
-
-  return [x for x in lst if x < int(num)]
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 num = input("Choose a number: ")
 print(less_than_num(a))
-
-
