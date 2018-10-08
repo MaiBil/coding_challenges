f488e1e9417946c5a3f8751f181c5975724c5507
MBilsky <mai.bilsky@gmail.com>
2018-10-08 01:21:17 -0300

    Add  small changes

--- practicepython.org/05-list-overlap.py
+++ practicepython.org/05-list-overlap.py
@@ -1,3 +1,12 @@
+#!/user/bin/env python
+
+# author: Mailen Bilsky
+# date: 04/10/2018
+
+# title: List Overlap
+# url: https://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+# time complexity: O(n), with n being the amount of numbers in the input string
+
 """
 Take two lists, say for example these two:
 
@@ -18,6 +27,14 @@ import random
 
 
 def common_list(list1, list2):
+    """
+    :param nmultiply: Two lists consisting of integers
+    :type multiply: list, list
+    :return: Returns a list containing only the elements
+    that are common between the lists (without duplicates)
+    by the user
+    :rtype: list
+    """
     if len(list1) > len(list2):
         ls = list1
     else:
@@ -26,6 +43,13 @@ def common_list(list1, list2):
 
 
 def random_list():
+    """
+    :param nmultiply: None
+    :type multiply: None
+    :return: Generates a list cointaining a random number of
+    elements, containing random numbers
+    :rtype: list
+    """
     return [random.randint(1, 100) for x in range(random.randint(1, 50))]
 
 
