c472f8d0696d73562bb386dd1b63ba0d901fafee
MBilsky <mai.bilsky@gmail.com>
2018-10-22 01:28:15 -0300

    Make changes requested. Move num variable inside function

--- practicepython.org/03-list-less-than-ten.py
+++ practicepython.org/03-list-less-than-ten.py
@@ -5,7 +5,6 @@
 
 # title: List Less Than Ten
 # url: https://www.practicepython.org/exercise/2014/02/15/03-list-less-than-ten.html
-# time complexity: O(n), with n being the amount of numbers in the input string
 
 """
 Take a list, say for example this one:
@@ -27,15 +26,23 @@ user.
 
 def less_than_num(lst):
     """
-    :param nmultiply: A list
-    :type multiply: list
+    :param less_than_num: A list
+    :type less_than_num: list
     :return: Returns a list with all the elements from the input list that
     are smaller than the number given by the user
     :rtype: string
     """
-    return [x for x in lst if x < int(num)]
+    while True:
+        try:
+            num = int(input("Choose a number: "))
+            break
+        except ValueError:
+            print("Oops!  That was no valid number.  Try again...")
+
+    return [x for x in lst if x < num]
 
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-num = input("Choose a number: ")
-print(less_than_num(a))
+
+if __name__ == '__main__':
+    print(less_than_num(a))
